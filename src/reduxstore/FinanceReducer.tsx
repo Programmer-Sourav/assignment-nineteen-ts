f65ba8f1dca1b652b436d0f84ff1476e615f95c7
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-10-13 23:20:07 +0530

    initial commit

---
+++ src/reduxstore/FinanceReducer.tsx
@@ -0,0 +1,13 @@
+export interface FinanceState {
+    incomeList: string[], 
+    expensesList: string[], 
+    savingsList: string[], 
+
+    totalIncome : 0;
+    totalExpense: 0;
+    totalSavings: 0;
+}
+
+const initialState : FinanceState =  {
+   
+}
